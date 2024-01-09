8c9f9bd5c17a81a899474e24530c9881252bf51b
jamiegibney <jamiegibneyuk@gmail.com>
2024-01-09 18:29:09 +0000

    replaced ~ with $HOME in sh scripts

--- links.sh
+++ links.sh
@@ -9,24 +9,24 @@ function create_link() {
     ln -si "$abs_path" "$2"
 }
 
-create_link ./bash/.bashprofile ~/.bashprofile
-create_link ./bash/.bashrc ~/.bashrc
+create_link ./bash/.bashprofile $HOME/.bashprofile
+create_link ./bash/.bashrc $HOME/.bashrc
 
-create_link ./clang/.clang-format ~/.clang-format
-create_link ./clang/.clang-tidy ~/.clang-tidy
+create_link ./clang/.clang-format $HOME/.clang-format
+create_link ./clang/.clang-tidy $HOME/.clang-tidy
 
-create_link ./fish/config.fish ~/.config/fish/config.fish
+create_link ./fish/config.fish $HOME/.config/fish/config.fish
 
-create_link ./kitty/kitty-startup.session ~/.config/kitty/kitty-startup.session
-create_link ./kitty/kitty.conf ~/.config/kitty/kitty.conf
+create_link ./kitty/kitty-startup.session $HOME/.config/kitty/kitty-startup.session
+create_link ./kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 
-create_link ./config-nvim ~/.config/nvim
+create_link ./config-nvim $HOME/.config/nvim
 
-create_link ./tmux/.tmux.conf ~/.tmux.conf
+create_link ./tmux/.tmux.conf $HOME/.tmux.conf
 
-create_link ./wezterm/.wezterm.lua ~/.wezterm.lua
+create_link ./wezterm/.wezterm.lua $HOME/.wezterm.lua
 
-create_link ./zsh/.zshenv ~/.zshenv
-create_link ./zsh/.zshrc ~/.zshrc
+create_link ./zsh/.zshenv $HOME/.zshenv
+create_link ./zsh/.zshrc $HOME/.zshrc
 
 echo "Done"
