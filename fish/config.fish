2500223fc934ff5ff3659ffda283e9df30f4b942
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-08-01 23:09:52 +0100

    config: updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,12 @@ end
 set -U fish_greeting
 
 function fish_prompt
-    printf '%s %s%s%s%s > ' (set_color cyan -o) (prompt_pwd --dir-length=0) (set_color normal) (fish_git_prompt) (set_color cyan -o)
+    set -g __fish_git_prompt_show_informative_status false
+    set -g __fish_git_prompt_showuntrackedfiles false
+    set -g __fish_git_prompt_showdirtystate false
+    set -g __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_showupstream informative
+    printf '%s %s%s%s%s > ' (set_color blue -o) (prompt_pwd --dir-length=0) (set_color normal) (fish_git_prompt) (set_color blue -o)
 end
 
 set -g fish_vi_key_bindings
@@ -23,9 +28,10 @@ abbr --add oh open . # "Open Here" (in Finder)
 
 # Git
 abbr --add gs git status
-abbr --add gaa git add -A
-abbr --add gc git commit -m
+abbr --add gf git fetch
+abbr --add gaa git add -A # "Git add all"
 abbr --add ghv gh repo view --web # "GitHub View"
+abbr --add lg lazygit
 
 # Vim
 alias vi=vim
@@ -57,6 +63,7 @@ abbr --add cnt cargo +nightly test
 abbr --add dot cd ~/.dotfiles/
 abbr --add drop cd ~/Library/CloudStorage/Dropbox/
 abbr --add dev cd ~/Documents/dev/
+abbr --add pro cd ~/Documents/dev/pro/
 abbr --add rust cd ~/Documents/dev/Rust/
 abbr --add aoc cd ~/Documents/dev/Rust/adventofcode/ # "Advent of Code"
 abbr --add rpg cd ~/Documents/dev/Rust/playground/ # "Rust PlayGround"
@@ -65,19 +72,19 @@ abbr --add cpp cd ~/Documents/dev/C++/
 abbr --add ncfg cd ~/.config/nvim/ # "Neovim ConFiG"
 
 # Projects
-abbr --add sf cd ~/Documents/dev/C++/SPECTRAL_FILTER # "Spectral Filter"
+abbr --add pasf cd ~/Documents/dev/C++/pasf/ # "Spectral Filter"
 abbr --add ccp cd ~/Documents/dev/Rust/Uni/creative_coding/creative_coding_project/ # "Creative Coding Project"
+abbr --add srs cd ~/Documents/dev/pro/controlled_frenzy/sound_reactions_system/repo/
+abbr --add courts cd ~/Documents/Unity/CYN-Courts\\ \\\(1\\\)/Assets/Scripts/
+abbr --add cfmod cd ~/Documents/Unity/CYN-Courts\\ FMOD/cyn-courts-fmod2_03/
 
 # Uni
 abbr --add uni cd ~/Desktop/Uni/
 abbr --add y2 cd ~/Desktop/Uni/YEAR\\ 2/ # "Year 2"
 abbr --add y3 cd ~/Desktop/Uni/YEAR\\ 3/ # "Year 3"
-abbr --add mw cd ~/Desktop/Uni/YEAR\\ 2/Middleware/ # "Middleware"
-abbr --add rp cd ~/Desktop/Uni/YEAR\\ 2/Research\\ Practice/ # "Research Practice"
-abbr --add ip cd ~/Desktop/Uni/YEAR\\ 2/Industry\\ Practice/ # "Industry Practice"
-abbr --add ip2 cd ~/Documents/Unity/SpaceStation\\ FMOD/Assets/ # "Industry Practice project #2"
-abbr --add courts cd ~/Documents/Unity/CYN-Courts\\ \\\(1\\\)/Assets/Scripts/
-abbr --add cfmod cd ~/Documents/Unity/CYN-Courts\\ FMOD/cyn-courts-fmod2_03/
+abbr --add in cd ~/Desktop/Uni/YEAR\\ 3/Innovation/ # "Year 3"
+abbr --add pd cd ~/Desktop/Uni/YEAR\\ 3/Product\\ Design/ # "Year 3"
+abbr --add rp cd ~/Desktop/Uni/YEAR\\ 3/Research\\ Project/ # "Year 3"
 
 # Resume suspended process
 bind \cz "fg"
@@ -90,5 +97,5 @@ bind \cz "fg"
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
 
-export CXX=/Library/Developer/CommandLineTools/usr/bin/clang++
-export CC=/Library/Developer/CommandLineTools/usr/bin/clang
+# export CXX=/Library/Developer/CommandLineTools/usr/bin/clang++
+# export CC=/Library/Developer/CommandLineTools/usr/bin/clang
